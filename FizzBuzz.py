ae5bfa7422dd5b86242454a50d4ce82b6d985546
Barabsus <41168485+Barabsus@users.noreply.github.com>
2019-06-14 16:47:55 -0500

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,22 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 22 2016, 17:23:13) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+def fizz_buzz():
+    x= range(1,101)
+    for i in x:
+        if i % 15 == 0:
+            print ('fizzbuzz')
+        elif i%3 == 0:
+            print ("fizz")
+        elif i%5 == 0:
+            print ('buzz')
+        else:
+            print (i)
+  
+
+
+    
+    
